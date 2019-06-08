4f2f423f32aaa56355b04464a1d4fe6ddd3e6ad2
RajKumar <rajkumar@novastrid.com>
2019-06-08 16:59:02 +0530

    changes added

--- app/src/main/java/com/sample/program/sample_array_functions/Activity/AddUserActivity.java
+++ app/src/main/java/com/sample/program/sample_array_functions/Activity/AddUserActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.sample.program.sample_array_functions.Custom.MyEditTextLight;
 import com.sample.program.sample_array_functions.Custom.MyTextViewMedium;
@@ -14,15 +15,17 @@ import com.sample.program.sample_array_functions.Utils.CommonUtils;
 import java.util.Arrays;
 import java.util.List;
 
-public class AddUserActivity extends BaseActivity {
+public class AddUserActivity extends BaseActivity implements View.OnClickListener {
 
-    MyTextViewMedium txt_type;
+    MyTextViewMedium txt_type,weeks;
     Activity mActivity;
     MyEditTextLight days;
+    ImageView leftArrow,rightArrow;
 
 
 
-    String[] stringarray=new String[]{"sss", "type 2"};
+    String[] stringarray = new String[]{"sss", "type 2"};
+    String[] stringweek = new String[]{"sss", "type 2"};
 
     List<String> list = Arrays.asList(stringarray);
 
@@ -31,51 +34,84 @@ public class AddUserActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
-        mActivity=this;
+        mActivity = this;
         initView();
     }
 
     private void initView() {
-        txt_type=(MyTextViewMedium)findViewById(R.id.txt_type);
-        days=(MyEditTextLight) findViewById(R.id.days);
-
-
-
-        txt_type.setOnClickListener(new View.OnClickListener() {
+        txt_type = (MyTextViewMedium) findViewById(R.id.txt_type);
+        weeks = (MyTextViewMedium) findViewById(R.id.weeks);
+        days = (MyEditTextLight) findViewById(R.id.days);
+        leftArrow = (ImageView) findViewById(R.id.leftarrow);
+        rightArrow = (ImageView) findViewById(R.id.rightarrow);
+        leftArrow.setOnClickListener(this);
+        rightArrow.setOnClickListener(this);
+        txt_type.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                    CommonUtils.showPopmenu(mActivity, txt_type, stringarray);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (s.toString().equalsIgnoreCase("sss")) {
+                    days.setText("20%");
+                    days.setEnabled(false);
+
+                } else {
+                    days.setText("");
+                    days.setEnabled(true);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
 
-   txt_type.addTextChangedListener(new TextWatcher() {
-       @Override
-       public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-       }
 
-       @Override
-       public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-           if(s.toString().equalsIgnoreCase("sss")){
-               days.setText("20%");
-               days.setEnabled(false);
 
-           }else {
-               days.setText("");
-               days.setEnabled(true);
-           }
+    }
 
-       }
+    @Override
+    public void onClick(View v) {
 
-       @Override
-       public void afterTextChanged(Editable s) {
+        switch (v.getId()){
+            case R.id.txt_type:{
+                CommonUtils.showPopmenu(mActivity, txt_type, stringarray);
+                break;
+            }
+            case R.id.rightarrow:{
+              int currentvalue=Integer.parseInt(weeks.getText().toString());
 
-       }
-   });
+              if(currentvalue!=52){
+                  currentvalue=currentvalue+1;
+              }
 
+              weeks.setText(""+currentvalue);
+
+                break;
+            }
+
+            case R.id.leftarrow:{
+
+                int currentvalue=Integer.parseInt(  weeks.getText().toString());
+
+                if(currentvalue!=1){
+                    currentvalue=currentvalue-1;
+                }
+
+                weeks.setText(""+currentvalue);
+
+                break;
+
+            }
+        }
 
     }
 }
---
+++ app/src/main/java/com/sample/program/sample_array_functions/Custom/MyTextViewStyleFont.java
@@ -0,0 +1,100 @@
+package com.sample.program.sample_array_functions.Custom;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.util.AttributeSet;
+
+import com.sample.program.sample_array_functions.R;
+import com.sample.program.sample_array_functions.Utils.CommonUtils;
+
+
+public class MyTextViewStyleFont extends android.support.v7.widget.AppCompatTextView {
+    Context context;
+
+    public MyTextViewStyleFont(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+        init();
+        this.setLinkTextColor(ContextCompat.getColor(context, R.color.delred));
+
+    }
+
+    public MyTextViewStyleFont(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+
+        init();
+        this.setLinkTextColor(ContextCompat.getColor(context, R.color.delred));
+
+    }
+
+    public MyTextViewStyleFont(Context context) {
+        super(context);
+        this.context = context;
+        init();
+        this.setLinkTextColor(ContextCompat.getColor(context, R.color.delred));
+    }
+
+    @SuppressLint("WrongConstant")
+    public void init() {
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Font-Bold.ttf");
+        //setTextColor(getResources().getColor(R.color.text_grey));
+        setTypeface(tf, 0);
+
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+
+//        Spannable s = getCustomSpannableString(getContext(), text);
+
+        String word = "ISI";
+
+        String fulltext = String.valueOf(text);
+
+        if (fulltext.contains(word)) {
+//            setMovementMethod(LinkMovementMethod.getInstance());
+//
+//            ClickableSpan privacy = new ClickableSpan() {
+//                @Override
+//                public void updateDrawState(@NotNull TextPaint ds) {
+//                    super.updateDrawState(ds);
+//                    ds.setUnderlineText(false);
+//                    ds.setColor(getCurrentTextColor());
+//                }
+//
+//                @Override
+//                public void onClick(@NotNull View widget) {
+//
+//                    if (context != null) {
+//                        CommonUtils.showToastMessage(context.getApplicationContext(), "Clicked text");
+//                    }
+//
+//                }
+//            };
+//
+//            super.setText(addClickablePart(String.valueOf(text), word, privacy), BufferType.SPANNABLE);
+
+            Spanned result;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                result = Html.fromHtml(fulltext, Html.FROM_HTML_MODE_LEGACY);
+            } else {
+                result = Html.fromHtml(fulltext);
+            }
+            setMovementMethod(LinkMovementMethod.getInstance());
+            CommonUtils.removeUnderlines((Spannable) result);
+
+            super.setText(result, type);
+        } else {
+            super.setText(text, type);
+        }
+    }
+
+}
+
